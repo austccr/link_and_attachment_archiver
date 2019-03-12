ff9815989e2ce497ffc168ea192c8748987f5f71
Luke Bacon <luke@equivalentideas.com>
2019-03-12 13:04:01 +0100

    Add another test case of the full integration

--- spec/link_archiver_spec.rb
+++ spec/link_archiver_spec.rb
@@ -34,5 +34,52 @@ RSpec.describe LinkArchiver do
         ]
       end
     end
+
+    context 'example_09.html' do
+      let(:html) { File.read('./spec/fixtures/example_09.html') }
+
+      it 'pings archive.or for all the present links' do
+        archiver = LinkArchiver.new(
+          source_url: 'http://minerals.org.au/new_report_backs_carbon_capture_and_storage_in_australia'
+        )
+
+        VCR.use_cassette('internet_archive', record: :once) do
+          archiver.parse_html_and_archive_links(html)
+        end
+
+        expect(archiver.links).to eq [
+          {
+            errors: nil,
+            syndication: 'https://web.archive.org/web/20190312114818/http://www.minerals.org.au/file_upload/files/publications/UQE003_CCS_report_HR.PDF',
+            url: 'http://www.minerals.org.au/file_upload/files/publications/UQE003_CCS_report_HR.PDF'
+          },
+          {
+            errors: '404: ',
+            syndication: nil,
+            url: 'http://minerals.org.au/UQE003_CCS_report_HR.PDF'
+          },
+          {
+            errors: '404: ',
+            syndication: nil,
+            url: 'http://minerals.org.au/WP1_Energy_and_Climate.pdf'
+          },
+          {
+            errors: nil,
+            syndication: 'https://web.archive.org/web/20190312114821/http://www.minerals.org.au/file_upload/files/publications/WP1_Energy_and_Climate.pdf',
+            url: 'http://www.minerals.org.au/file_upload/files/publications/WP1_Energy_and_Climate.pdf'
+          },
+          {
+            errors: nil,
+            syndication: 'https://web.archive.org/web/20190312114822/http://www.minerals.org.au/file_upload/files/publications/WP2_Financial_Incentives.pdf',
+            url: 'http://www.minerals.org.au/file_upload/files/publications/WP2_Financial_Incentives.pdf'
+          },
+          {
+            errors: nil,
+            syndication: 'https://web.archive.org/web/20190312114823/http://www.minerals.org.au/file_upload/files/publications/WP3_CCS_Roadmaps_and_Projects.pdf',
+            url: 'http://www.minerals.org.au/file_upload/files/publications/WP3_CCS_Roadmaps_and_Projects.pdf'
+          }
+        ]
+      end
+    end
   end
 end
