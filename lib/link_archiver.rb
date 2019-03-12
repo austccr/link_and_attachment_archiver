b30127d7e0f6b29d8263756ddd370373a3eec45d
Luke Bacon <luke@equivalentideas.com>
2019-03-12 12:09:58 +0100

    Extract archive_links to file for consistency

--- lib/link_archiver.rb
+++ lib/link_archiver.rb
@@ -1,4 +1,5 @@
 require_relative 'link_archiver/parse_html.rb'
+require_relative 'link_archiver/archive_links.rb'
 
 class LinkArchiver
   attr_accessor :links
@@ -7,44 +8,5 @@ class LinkArchiver
   def initialize(source_url:)
     @links = []
     @source_url = source_url
-  end
-
-  def archive_links
-    links.each_with_index do |link,i|
-      links[i] = link.merge!(web_archive(link[:url]))
-    end
-  end
-
-  private
-
-  def web_archive(url)
-    response_details = {
-      syndication: nil,
-      errors: nil
-    }
-
-    request = Typhoeus::Request.new(
-      "https://web.archive.org/save/#{url}",
-      method: :head
-    )
-
-    request.on_complete do |response|
-      if response.success?
-        response_details[:syndication] = [
-          "https://web.archive.org",
-          response.headers['content-location']
-        ].join
-      else
-        response_details[:errors] = [
-          response.code.to_s + ': ',
-          response.status_message,
-          response.headers.dig('X-Archive-Wayback-Runtime-Error')
-        ].join
-      end
-    end
-
-    request.run
-
-    response_details
   end
 end
---
+++ lib/link_archiver/archive_links.rb
@@ -0,0 +1,40 @@
+class LinkArchiver
+  def archive_links
+    links.each_with_index do |link,i|
+      links[i] = link.merge!(web_archive(link[:url]))
+    end
+  end
+
+  private
+
+  def web_archive(url)
+    response_details = {
+      syndication: nil,
+      errors: nil
+    }
+
+    request = Typhoeus::Request.new(
+      "https://web.archive.org/save/#{url}",
+      method: :head
+    )
+
+    request.on_complete do |response|
+      if response.success?
+        response_details[:syndication] = [
+          "https://web.archive.org",
+          response.headers['content-location']
+        ].join
+      else
+        response_details[:errors] = [
+          response.code.to_s + ': ',
+          response.status_message,
+          response.headers.dig('X-Archive-Wayback-Runtime-Error')
+        ].join
+      end
+    end
+
+    request.run
+
+    response_details
+  end
+end
