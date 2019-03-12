981859106d46cc43e1ccab7bc8c2b2d11ab48486
Luke Bacon <luke@equivalentideas.com>
2019-03-12 10:25:11 +0100

    Add more examples

--- spec/link_archiver_spec.rb
+++ spec/link_archiver_spec.rb
@@ -105,5 +105,138 @@ RSpec.describe LinkArchiver do
         ]
       end
     end
+
+    context 'example_02.html' do
+      let(:html) { File.read('./spec/fixtures/example_02.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/exceptional-victorian-women-resources-sought-2019-awards'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'http://minerals.org.au/events/victorian-women-resources-awards-2019' },
+          { url: 'http://minerals.org.au/files/190308-exceptional-victorian-women-resources-sought-2019-awardspdf-0' },
+          { url: 'https://minerals.org.au/sites/default/files/190308%20Exceptional%20Victorian%20women%20in%20resources%20sought%20for%202019%20awards_0.pdf' }
+        ]
+      end
+    end
+
+    context 'example_03.html' do
+      let(:html) { File.read('./spec/fixtures/example_03.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/powering-future-australian-mining-people-innovation-and-modern-education'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'https://www.minerals.org.au/news/future-work-changing-skills-landscape-miners' },
+          { url: 'https://www.minerals.org.au/news/future-work-economic-implications-technology-and-digital-mining' },
+          { url: 'http://minerals.org.au/files/190214-powering-future-australian-mining-people-innovation-and-modern-educationpdf' },
+          { url: 'https://minerals.org.au/sites/default/files/190214%20Powering%20the%20future%20of%20Australian%20mining%20with%20people%20innovation%20and%20modern%20education.pdf' }
+        ]
+      end
+    end
+
+    context 'example_04.html' do
+      let(:html) { File.read('./spec/fixtures/example_04.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/new-campaign-shows-there%E2%80%99s-more-australian-mining'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'https://www.youtube.com/watch?v=uDgGTjJ1KnY' },
+          { url: 'http://minerals.org.au/files/190211-new-campaign-shows-theres-more-australian-miningpdf-0' },
+          { url: 'https://minerals.org.au/sites/default/files/190211%20New%20campaign%20shows%20there%27s%20more%20to%20Australian%20mining_0.pdf' }
+        ]
+      end
+    end
+
+    context 'example_05.html' do
+      let(:html) { File.read('./spec/fixtures/example_05.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/tpp-11-ratification-great-news-australian-jobs-and-workers'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'http://www.minerals.org.au/sites/default/files/180905%20Australia%20will%20gain%20from%20continued%20Asia-Pacific%20trade%20integration.pdf' },
+          { url: 'https://www.lowyinstitute.org/publications/2017-lowy-institute-poll#section_32816' },
+          { url: 'http://minerals.org.au/files/181031-tpp-11-ratification-great-news-australian-jobs-and-workerspdf' },
+          { url: 'http://minerals.org.au/sites/default/files/181031%20TPP-11%20ratification%20great%20news%20for%20Australian%20jobs%20and%20workers.pdf' }
+        ]
+      end
+    end
+
+    context 'example_06.html' do
+      let(:html) { File.read('./spec/fixtures/example_06.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/australia%E2%80%99s-world-class-minerals-industry-shows-global-leadership-sustainability'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'https://www.minerals.org.au/sites/default/files/Sustainability%20in%20Action%20October%202018%20WEB_0.pdf' },
+          { url: 'http://minerals.org.au/files/181030-australia%E2%80%99s-world-class-minerals-industry-shows-global-leadership-sustainabilitypdf' },
+          { url: 'http://minerals.org.au/sites/default/files/181030%20Australia%E2%80%99s%20world-class%20minerals%20industry%20shows%20global%20leadership%20on%20sustainability.pdf' },
+          { url: 'http://minerals.org.au/files/sustainability-action-report-october-2018pdf' },
+          { url: 'http://minerals.org.au/sites/default/files/Sustainability%20in%20Action%20October%202018%20WEB_0.pdf' }
+        ]
+      end
+    end
+
+    context 'example_07.html' do
+      let(:html) { File.read('./spec/fixtures/example_07.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/austmine-mca-mou-strengthens-minings-sustainability'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'http://www.austmine.com.au' },
+          { url: 'http://minerals.org.au/file/420' },
+          { url: 'http://minerals.org.au/sites/default/files/Austmine_MCA_MOU_October_2013.pdf' }
+        ]
+      end
+    end
+
+    context 'example_08.html' do
+      let(:html) { File.read('./spec/fixtures/example_08.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/news/minister_canavan_on_target_in_supporting_coal_industry_and_jobs'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'http://minerals.org.au/app/private/Media_statement_-_Minister_Canavan_on_target_in_supporting_coal_industry_and_jobs_5_Jan_2017%20%281%29.pdf' }
+        ]
+      end
+    end
+
+    context 'example_09.html' do
+      let(:html) { File.read('./spec/fixtures/example_09.html') }
+
+      before do
+        archiver.source_url = 'http://minerals.org.au/new_report_backs_carbon_capture_and_storage_in_australia'
+      end
+
+      it 'extracts URLs' do
+        is_expected.to eql [
+          { url: 'http://www.minerals.org.au/file_upload/files/publications/UQE003_CCS_report_HR.PDF' },
+          { url: 'http://minerals.org.au/UQE003_CCS_report_HR.PDF' },
+          { url: 'http://minerals.org.au/WP1_Energy_and_Climate.pdf' },
+          { url: 'http://www.minerals.org.au/file_upload/files/publications/WP1_Energy_and_Climate.pdf' },
+          { url: 'http://www.minerals.org.au/file_upload/files/publications/WP2_Financial_Incentives.pdf' },
+          { url: 'http://www.minerals.org.au/file_upload/files/publications/WP3_CCS_Roadmaps_and_Projects.pdf' }
+        ]
+      end
+    end
   end
 end
