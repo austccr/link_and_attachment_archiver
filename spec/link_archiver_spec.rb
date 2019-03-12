1a2c429059453b53fd97fbc6d503d0fd10874118
Luke Bacon <luke@equivalentideas.com>
2019-03-12 09:41:45 +0100

    Extract shared test setup

--- spec/link_archiver_spec.rb
+++ spec/link_archiver_spec.rb
@@ -10,59 +10,52 @@ RSpec.describe LinkArchiver do
   describe '#parse_html' do
     let(:archiver) { LinkArchiver.new(source_url: 'https://foo.org/bar') }
 
+    subject do
+      archiver.parse_html(html)
+      archiver.links
+    end
+
     context 'when there are no links' do
+      let(:html) { '<p>here is some html</p>' }
+
       it 'it finds none' do
-        html = '<p>here is some html</p>'
-
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql []
+        is_expected.to eql []
       end
     end
 
     context 'when there is a link' do
+      let(:html) { '<p>here is some html, with <a href="https://feministinternet.org/">a link</a></p>' }
+
       it 'takes a string of HTML and extracts the links' do
-        html = '<p>here is some html, with <a href="https://feministinternet.org/">a link</a></p>'
-
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
-          { url: 'https://feministinternet.org/' }
-        ]
+        is_expected.to eql [ { url: 'https://feministinternet.org/' } ]
       end
     end
 
     context 'with a relative URL' do
+      let(:html) { '<p>here is some html, with <a href="/files/foobar">a link</a></p>' }
+
       it 'it makes it absolute' do
-        html = '<p>here is some html, with <a href="/files/foobar">a link</a></p>'
-
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
+        is_expected.to eql [
           { url: 'https://foo.org/files/foobar' }
         ]
       end
     end
 
     context 'when there are duplicate links' do
+      let(:html) { '<p>here is some html, with <a href="https://feministinternet.org/">a link</a></p><div><a href="https://feministinternet.org/">a duplicate link</a></div>' }
+
       it 'takes a string of HTML and extracts the links' do
-        html = '<p>here is some html, with <a href="https://feministinternet.org/">a link</a></p><div><a href="https://feministinternet.org/">a duplicate link</a></div>'
-
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
+        is_expected.to eql [
           { url: 'https://feministinternet.org/' }
         ]
       end
     end
 
     context 'when there are two links' do
+      let(:html) { '<p>here is some html, with <a href="https://feministinternet.org/">a link</a></p><div><a href="https://www.forensic-architecture.org/">another link</a></div>' }
+
       it 'takes a string of HTML and extracts the links' do
-        html = '<p>here is some html, with <a href="https://feministinternet.org/">a link</a></p><div><a href="https://www.forensic-architecture.org/">another link</a></div>'
-
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
+        is_expected.to eql [
           { url: 'https://feministinternet.org/' },
           { url: 'https://www.forensic-architecture.org/' },
         ]
@@ -70,26 +63,28 @@ RSpec.describe LinkArchiver do
     end
 
     context 'duplicate_example.html' do
-      it 'extracts unique URLs' do
+      let(:html) { File.read('./spec/fixtures/duplicate_example.html') }
+
+      before do
         archiver.source_url = 'https://minerals.org.au/news/peru-australia-free-trade-agreement-will-deliver-investment-and-jobs-australia'
-        html = File.read('./spec/fixtures/duplicate_example.html')
+      end
 
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
+      it 'extracts unique URLs' do
+        is_expected.to eql [
           { url: 'https://minerals.org.au/sites/default/files/190226%20Peru-Australia%20Free%20Trade%20Agreement%20will%20deliver%20investment%20and%20jobs%20for%20Australia.pdf' }
         ]
       end
     end
 
     context 'example.html' do
-      it 'extracts URLs' do
+      let(:html) { File.read('./spec/fixtures/example.html') }
+
+      before do
         archiver.source_url = 'http://minerals.org.au/news/peru-australia-free-trade-agreement-will-deliver-investment-and-jobs-australia'
-        html = File.read('./spec/fixtures/example.html')
+      end
 
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
+      it 'extracts URLs' do
+        is_expected.to eql [
           { url: 'http://minerals.org.au/files/190226-peru-australia-free-trade-agreement-will-deliver-investment-and-jobs-australiapdf' },
           { url: 'https://minerals.org.au/sites/default/files/190226%20Peru-Australia%20Free%20Trade%20Agreement%20will%20deliver%20investment%20and%20jobs%20for%20Australia.pdf' }
         ]
@@ -97,13 +92,14 @@ RSpec.describe LinkArchiver do
     end
 
     context 'example_01.html' do
-      it 'extracts URLs' do
+      let(:html) { File.read('./spec/fixtures/example_01.html') }
+
+      before do
         archiver.source_url = 'http://minerals.org.au/news/mca-calls-parliament-support-regional-communities-and-jobs'
-        html = File.read('./spec/fixtures/example_01.html')
+      end
 
-        archiver.parse_html(html)
-
-        expect(archiver.links).to eql [
+      it 'extracts URLs' do
+        is_expected.to eql [
           { url: 'http://minerals.org.au/files/190220-mca-calls-parliament-support-regional-communities-and-jobspdf' },
           { url: 'https://minerals.org.au/sites/default/files/190220%20MCA%20calls%20on%20Parliament%20to%20support%20regional%20communities%20and%20jobs.pdf' }
         ]
