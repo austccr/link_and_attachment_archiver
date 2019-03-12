ba5cccd90f1f71a9d3c3ac3502c5cd27c6b54115
Luke Bacon <luke@equivalentideas.com>
2019-03-12 10:44:48 +0100

    Add pending #archive_links tests

--- spec/link_archiver_spec.rb
+++ spec/link_archiver_spec.rb
@@ -6,4 +6,32 @@ RSpec.describe LinkArchiver do
       raise_error(ArgumentError, 'missing keyword: source_url')
     )
   end
+
+  describe '#archive_links' do
+    let(:archiver) do
+      LinkArchiver.new(source_url: 'https://foo.org/bar')
+    end
+
+    pending 'sends the links to the internet archive' do
+      expect(archiver.links).to eq [
+        {
+          url: 'https://feministinternet.org/',
+          syndication: 'https://web.archive.org/web/201903120942/https://feministinternet.org/',
+          errors: nil
+        }
+      ]
+    end
+
+    context 'if there are errors' do
+      pending 'records them' do
+        expect(archiver.links).to eq [
+          {
+            url: 'https://feministinternet.org/',
+            syndication: '',
+            errors: 'error'
+          }
+        ]
+      end
+    end
+  end
 end
