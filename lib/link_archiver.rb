cc2c5cb7180fd73bf83f53a6b48ad9535be31ce8
Luke Bacon <luke@equivalentideas.com>
2019-03-12 08:16:02 +0100

    It works on a real world example

--- lib/link_archiver.rb
+++ lib/link_archiver.rb
@@ -1,19 +1,27 @@
 require 'nokogiri'
 
 class LinkArchiver
-  attr_reader :links
+  attr_accessor :links
   attr_accessor :source_url
 
   def initialize(source_url:)
     @links = []
-    @source_url = nil
+    @source_url = source_url
   end
 
   def parse_html(string)
+    extracted_links = []
+
     Nokogiri::HTML(string).search(:a).each do |a|
-      links << {
-        url: a[:href]
-      }
+      url = URI.parse(a[:href])
+
+      if url.is_a? URI::Generic
+        url = URI.join(source_url, url)
+      end
+
+      extracted_links << { url: url.to_s }
     end
+
+    self.links += extracted_links
   end
 end
--- spec/link_archiver_spec.rb
+++ spec/link_archiver_spec.rb
@@ -1,19 +1,15 @@
 require_relative '../lib/link_archiver'
 
 RSpec.describe LinkArchiver do
-  let(:archiver) { LinkArchiver.new(source_url: '') }
-
   it 'must have a source url' do
     expect { LinkArchiver.new }.to(
       raise_error(ArgumentError, 'missing keyword: source_url')
     )
   end
 
-  it 'has an attribute links' do
-    expect(archiver.links).to eql []
-  end
-
   describe '#parse_html' do
+    let(:archiver) { LinkArchiver.new(source_url: 'https://foo.org/bar') }
+
     context 'when there are no links' do
       it 'it finds none' do
         html = '<p>here is some html</p>'
@@ -45,6 +41,20 @@ RSpec.describe LinkArchiver do
         expect(archiver.links).to eql [
           { url: 'https://feministinternet.org/' },
           { url: 'https://www.forensic-architecture.org/' },
+        ]
+      end
+    end
+
+    context 'example.html' do
+      it 'extracts URLs' do
+        archiver.source_url = 'http://minerals.org.au/news/peru-australia-free-trade-agreement-will-deliver-investment-and-jobs-australia'
+        html = File.read('./spec/fixtures/example.html')
+
+        archiver.parse_html(html)
+
+        expect(archiver.links).to eql [
+          { url: 'http://minerals.org.au/files/190226-peru-australia-free-trade-agreement-will-deliver-investment-and-jobs-australiapdf' },
+          { url: 'https://minerals.org.au/sites/default/files/190226%20Peru-Australia%20Free%20Trade%20Agreement%20will%20deliver%20investment%20and%20jobs%20for%20Australia.pdf' }
         ]
       end
     end
