506f7f9815a1fb19f70b39cec90c07ef2e544e1d
Luke Bacon <luke@equivalentideas.com>
2019-04-03 14:23:30 +0200

    Extract method

--- scraper.rb
+++ scraper.rb
@@ -12,6 +12,32 @@ FEED_URLS = [
   MORPH_API_URL
 ]
 
+def save_links(archiver, totals_counter)
+  archiver.links.each do |link|
+    link.merge!(
+      source_url: archiver.source_url,
+      archived_at: Time.now.utc.to_s
+    )
+
+    # TODO: Attempt to re-archive and update the link if there are errors on the existing record
+    #
+    # This should probably be extracted into lib so that it can be done without pining archive.org redundantly
+    #
+    # Get the existing record with something like
+    # existing_link = ScraperWiki.select(
+    #   "* FROM data WHERE url='#{link[:url]}' AND source_url='#{link[:source_url]}'"
+    # ).last rescue nil
+
+    ScraperWiki.save_sqlite([:url, :source_url], link)
+
+    sleep 1
+
+    totals_counter += 1
+  end
+
+  totals_counter
+end
+
 def archive_links_from_morph_results(feed_url, current_offset, total_links, total_records)
   query = "select * from \"data\" limit #{PER_PAGE} offset #{current_offset}"
 
@@ -27,27 +53,7 @@ def archive_links_from_morph_results(feed_url, current_offset, total_links, tota
 
     archiver.parse_html_and_archive_links(record["content"])
 
-    archiver.links.each do |link|
-      link.merge!(
-        source_url: archiver.source_url,
-        archived_at: Time.now.utc.to_s
-      )
-
-      # TODO: Attempt to re-archive and update the link if there are errors on the existing record
-      #
-      # This should probably be extracted into lib so that it can be done without pining archive.org redundantly
-      #
-      # Get the existing record with something like
-      # existing_link = ScraperWiki.select(
-      #   "* FROM data WHERE url='#{link[:url]}' AND source_url='#{link[:source_url]}'"
-      # ).last rescue nil
-
-      ScraperWiki.save_sqlite([:url, :source_url], link)
-
-      total_links += 1
-
-      sleep 1
-    end
+    total_links = save_links(archiver, total_links)
 
     total_records += 1
   end
